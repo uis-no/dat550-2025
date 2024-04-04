f6eca68426709bbfaf80d760195a90cbd3013eba
vinaysetty <1608263+vinaysetty@users.noreply.github.com>
2024-04-04 16:33:42 +0200

    Update clip_train.py

--- handson_multimodal/clip_train.py
+++ handson_multimodal/clip_train.py
@@ -13,9 +13,7 @@ processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(text=["a photo of a cat", "cats and remotes"], images=image, return_tensors="pt", padding="max_length", max_length=77, truncation=True)
-
-# dataset = load_dataset("your_dataset_name")
+inputs = processor(text=["a photo of a cat", "a photo of a dog", "cats and remotes"], images=image, return_tensors="pt", padding="max_length", max_length=77, truncation=True)
 
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image # this is the image-text similarity score
