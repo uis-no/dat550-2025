a7111a329ddc082e72a096661d18ee5eb7b5c95e
vinaysetty <1608263+vinaysetty@users.noreply.github.com>
2024-04-05 18:31:35 +0200

    Update imdb_train.sh

--- handson_gpu_usage/slurm/imdb_train.sh
+++ handson_gpu_usage/slurm/imdb_train.sh
@@ -8,7 +8,7 @@
 # Activate environment
 uenv verbose cuda-12.2.0 cudnn-12.x-8.8.0
 uenv miniconda3-py39
-conda activate slurm_cuda12
+conda activate transformer_cuda12
 PATH=~/.local/bin:$PATH
 echo $PATH
 # Run the Python script that uses the GPU
