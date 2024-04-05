69bb34cd88c63da312ba1729d09f9f1976fe425b
vinaysetty <1608263+vinaysetty@users.noreply.github.com>
2024-04-05 18:31:21 +0200

    Update conda_setup.sh

--- handson_gpu_usage/slurm/conda_setup.sh
+++ handson_gpu_usage/slurm/conda_setup.sh
@@ -8,7 +8,7 @@
 uenv verbose cuda-12.2.0 cudnn-12.x-8.8.0
 uenv miniconda3-py39
 conda create -n transformer_cuda12 -c pytorch pytorch torchvision torchaudio pytorch-cuda=12.1 -c nvidia -y
-conda activate slurm_cuda12
+conda activate transformer_cuda12
 pip3 install torch torchvision torchaudio
 pip3 install transformers[torch]
 pip3 install -r requirements.txt
