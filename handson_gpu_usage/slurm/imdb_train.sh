9be19bc6e166d6b0af38da30070f15d2b28af225
vinaysetty <1608263+vinaysetty@users.noreply.github.com>
2025-03-29 18:36:48 +0100

    Update imdb_train.sh

--- handson_gpu_usage/slurm/imdb_train.sh
+++ handson_gpu_usage/slurm/imdb_train.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-#SBATCH --partition=gpuA100 
-#SBATCH --time=1:00:00
-#SBATCH --job-name=imdb_train
-#SBATCH --output=imdb_train.out
- 
+#SBATCH --partition=gpu
+#SBATCH --time=2:00:00
+#SBATCH --mem=100G
+#SBATCH --job-name=claim_new_data
+#SBATCH --output=out/claim_singletask_new_data_only_en.out
+#SBATCH --nodelist=gorina9
+
+
+
 # Activate environment
-uenv verbose cuda-12.2.0 cudnn-12.x-8.8.0
-uenv miniconda3-py39
-conda activate transformer_cuda12
-PATH=~/.local/bin:$PATH
-echo $PATH
-# Run the Python script that uses the GPU
-TOKENIZERS_PARALLELISM=false python -u bert_train.py
+uenv verbose cuda-12.4.0 cudnn-12.x-8.8.0
+uenv miniconda3-py311
+# conda create -n llama_ft_unsloth python=3.9 -y
+# pip install bitsandbytes transformers accelerate peft trl
+CUDA_LAUNCH_BLOCKING=1 TOKENIZERS_PARALLELISM=false python -u bert_train.py
